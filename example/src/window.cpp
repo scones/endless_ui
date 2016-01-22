60a9c7e7644306502eb52f85f467505f984ddb96
Dirk Gustke <code@asm68k.org>
2016-01-22 17:06:37 +0100

    master - added some logic

--- example/src/window.cpp
+++ example/src/window.cpp
@@ -6,10 +6,31 @@
  */
 
 
+#include "core/window.h"
+#include "core/glfw.h"
+
+
 int main(void) {
 
+  core::glfw glfw;
+  glfw.init();
 
+  core::window window("window example");
+  window.set_position({20, 20});
+  window.set_resolution({500, 500});
+  window.set_fullscreen(false);
+  window.set_window_hints({
+    {GLFW_SAMPLES, 4},
+    {GLFW_CONTEXT_VERSION_MAJOR, 4},
+    {GLFW_CONTEXT_VERSION_MINOR, 5}
+  });
+  window.init();
 
+  do {
+    glfwPollEvents();
+
+    window.swap_buffers();
+  } while (!window.should_close());
   return 1;
 }
 
